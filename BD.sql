ddd049fb5ac2e84e32fb8c9bbb609ba30e552d5d
luis229822 <luis22199822@gmail.com>
2025-02-26 10:09:05 -0500

    Update BD.sql

--- BD.sql
+++ BD.sql
@@ -1,5 +1,5 @@
-CREATE SCHEMA IF NOT EXISTS BD;
-USE BD;
+CREATE SCHEMA IF NOT EXISTS bd;
+USE bd;
 
 DROP TABLE IF EXISTS s_cliente;
 CREATE TABLE s_cliente 
@@ -52,7 +52,7 @@ INSERT INTO s_cliente VALUES ('203', 'New Delhi Sports', '91-11903338','11368 Ch
 'New Delhi', NULL, 'India', NULL,'BUENO', '11', '4', NULL);
 INSERT INTO s_cliente VALUES ('204', 'Ladysport', '1-206-104-0111','281 Queen Street',
 'Seattle', 'Washington', 'US', NULL,'EXCELENTE', '11', '1', NULL);
-INSERT INTO s_cliente VALUES ('205', 'Kim''s Sporting BUENOs', '852-3693888','15 Henessey Road',
+INSERT INTO s_cliente VALUES ('205', 'Kim', 's Sporting BUENOs', '852-3693888','15 Henessey Road',
 'Hong Kong', NULL, NULL, NULL,'EXCELENTE', '11', '4', NULL);
 INSERT INTO s_cliente VALUES ('206', 'Sportique', '33-93425722253','172 Rue de Place',
 'Cannes', NULL, 'France', NULL,'EXCELENTE', '13', '5', NULL);
